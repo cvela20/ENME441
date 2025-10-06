4d4f2ba952daf3850a05e252042e2175a2a86b66
cvela20 <velacameron20@gmail.com>
2025-10-06 19:43:20 -0400

    Add files via upload

--- Lab4.py
+++ Lab4.py
@@ -7,18 +7,25 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-p = 17
+p1 = 17
+p2 = 27
 f = 0.2
+phi = math.pi/11
+
 GPIO.setup(p, GPIO.OUT, initial=0)
 tstart = time.time()
-pwm = GPIO.PWM(p, 500)
-pwm.start(0)
+pwm1 = GPIO.PWM(p1, 500)
+pwm2 = GPIO.PWM(p2, 500)
+pwm1.start(0)
+pwm2.start(0)
 
 try:
 	while True:
 		t = time.time() - tstart
-		B = math.sin(2*(math.pi)*f*t)**2
-		pwm.ChangeDutyCycle(B*100)
+		B1 = math.sin(2*(math.pi)*f*t)**2
+		B2 = math.sin(2*(math.pi)*f*t-phi)**2
+		pwm1.ChangeDutyCycle(B1*100)
+		pwm2.ChangeDutyCycle(B2*100)
 
 except KeyboardInterrupt:
 	print('\nExiting')
