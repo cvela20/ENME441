83d5122fdb9d2c11729d0812d5336694759586d6
cvela20 <velacameron20@gmail.com>
2025-10-20 00:50:23 -0400

    Add files via upload

---
+++ bug.py
@@ -0,0 +1,36 @@
+from BugClass import Bug
+import RPi.GPIO as GPIO
+import time, random
+
+GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+dt = 0.1
+bug = Bug(timestep=dt, x=3, isWrapOn=False)
+s2 = 0
+
+try:
+	while True:
+		previous_s2 = s2
+		s1 = GPIO.input(17)
+		s2 = GPIO.input(27)
+		s3 = GPIO.input(22)
+
+	if s1 == True:
+		bug.start()
+	elif s1 == False:
+		bug.stop()
+
+	if s2 != previous_s2:
+		bug.isWrapOn = not bug.isWrapOn
+		previous_s2 = s2 
+
+
+	if s3 == True:
+		bug.timestep = dt/3.0
+		dt /= 3.0
+
+except KeyboradInterrupt:
+	pass
+finally:
+	GPIO.cleanup()
