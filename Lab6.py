e5535ff4c504063f4d1ee170bfbcdecdab20fa98
cvela20 <velacameron20@gmail.com>
2025-10-19 23:27:26 -0400

    Add files via upload

--- Lab6.py
+++ Lab6.py
@@ -4,18 +4,18 @@ import time, random
 
 sr = Shifter(serialPin=23, clockPin=25, latchPin=24)
 position = 0
+
 try:
-    while True:
-        led = 1 << position 
-        sr.shiftByte(led)
+    while True: 
+        sr.shiftByte(1 << position)
         time.sleep(0.05)
 
         step = random.choice([-1,1])
         position += step
         if position < 0:
-        	position = 0
+        	position = 1
         elif position > 7:
-        	position = 7
+        	position = 6
 
 except KeyboardInterrupt:
     pass
