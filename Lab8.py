61a1e6979c2ab3893335a602533b0cf24a6bacc9
cvela20 <velacameron20@gmail.com>
2025-10-29 01:15:43 -0400

    Add files via upload

--- Lab8.py
+++ Lab8.py
@@ -1,49 +1,24 @@
-# Web interface for GPIO control using POST request
-#
-# Display a user-defined binary value on an LED bar
+#Cameron Vela Lab 8 Question 1
 
-
-import RPi.GPIO as gpio
-import threading
-from time import sleep
 import socket
+from RPi import GPIO
 
-from shifter import Shifter    # Use our custom Shifter class
+#Set up output pins and pwm signals going to output pins
+GPIO.setmode(GPIO.BCM)
+pins = [17,27,22]
 
-dataPin, latchPin, clockPin = 23, 24, 25
-sh = Shifter(dataPin, latchPin, clockPin)
+for p in pins:
+    GPIO.setup(p, GPIO.OUT)
 
-def web_page(led_byte):
-    # Define html code, with user text passed from the browser via POST request.
-    # Note we cannot use an f-string here since there are HTML style definitions
-    # that use the {} syntax!
-    html = """
-    <html><head><title>LED bar display</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <head>
-    <style>
-    html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-    h1{color: #0F3376; padding: 2vh;}
-    p{font-size: 1.5rem;}
-    .button{display: inline-block; background-color: #e7bd3b; border: none; border-radius: 4px; color: white;
-                     padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-    </style>
-    </head>
-    <body>
-    <h1>Web Server Test</h1> 
-    <p>Byte value to display on LED bar:</p>
-    <p><strong>""" + led_byte + """</strong> (base 10)</p>
-    <p><strong>""" + bin(int(led_byte))[2:] + """</strong> (base 2)</p>
-    <form action="/" method="POST">
-      <p><input type="text" name="led_byte"> 
-      <p><button type="submit" class="button" name="submit" value="">Display Byte</button></p>
-    </form>
-    </body>
-    </html>
-    """
-    return bytes(html, 'utf-8')
+f = 500
 
-# Helper function to extract key,value pairs of POST data
+pwms = [GPIO.PWM(p, 500) for p in pins]
+
+for pwm in pwms:
+    pwm.start(0)
+brightness = [0, 0, 0]
+
+# Parse function from class
 def parsePOSTdata(data):
     data_dict = {}
     idx = data.find('\r\n\r\n')+4
@@ -55,43 +30,60 @@ def parsePOSTdata(data):
             data_dict[key_val[0]] = key_val[1]
     return data_dict
 
+def web_page():
+    html = f"""
+    <html>
+<head>
+<title>LED Brightness Control</title>
+</head>
+<body>
+
+  <p1>Brightness Level:</p1>
+  <form action="/cgi-bin/led_control.py" method="POST">
+  <input type="range" name="slider1" min="0" max="100" value="25">
+
+  <p>Select LED:</p>
+  <input type="radio" name="option" value="1" checked> LED 1 ({brightness[0]}%)<br>
+  <input type="radio" name="option" value="2"> LED 2 ({brightness[1]}%)<br>
+  <input type="radio" name="option" value="3"> LED 3 ({brightness[2]}%)<br>
+
+  <input type="submit" value="Change Brightness">
+  </form>
+
+</body>
+</html>
+"""
+return html.encode('utf-8')
 
 def serve_web_page():
     while True:
         print('Waiting for connection...')
-        conn, (client_ip, client_port) = s.accept()     # blocking call
+        conn, (client_ip, client_port) = s.accept() 
         print(f'Connection from {client_ip} on client port {client_port}')
         client_message = conn.recv(2048).decode('utf-8')
         print(f'Message from client:\n{client_message}')
         data_dict = parsePOSTdata(client_message)
-        if 'led_byte' in data_dict.keys():   # make sure data was posted
-            led_byte = data_dict["led_byte"]
-        else:   # web page loading for 1st time so start with 0 for the LED byte
-            led_byte = '0'
+
+        if 'slider1' in data_dict.keys() and 'option' in data_dict.keys():   # make sure data was posted
+            led_brightness = int(data_dict["slider1"])
+            led_number = int(data_dict["option"])
+            if led_number == 1:
+                brightness[0] = led_brightness
+                pwms[0].ChangeDutyCycle(brightness[0])
+            elif led_number == 2:
+                brightness[1] = led_brightness
+                pwms[1].ChangeDutyCycle(brightness[1])
+            elif led_number == 3:
+                brightness[2] = led_brightness
+                pwms[2].ChangeDutyCycle(brightness[2])
+
         conn.send(b'HTTP/1.1 200 OK\r\n')                  # status line
         conn.send(b'Content-Type: text/html\r\n')          # headers
         conn.send(b'Connection: close\r\n\r\n')   
-        try:
-            conn.sendall(web_page())                       # body
-        finally:
-            conn.close()
+        conn.sendall(web_page())
 
-        sh.shiftByte(int(led_byte))    # display byte on the LED bar
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 8080))
+s.listen(1)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # pass IP addr & socket type
-s.bind(('', 80))     # bind to given port
-s.listen(3)          # up to 3 queued connections
-
-webpageTread = threading.Thread(target=serve_web_page)
-webpageTread.daemon = True
-webpageTread.start()
-
-# Do whatever we want while the web server runs in a separate thread:
-try:
-    while True:
-        pass
-except:
-    print('Joining webpageTread')
-    webpageTread.join()
-    print('Closing socket')
-    s.close()
+serve_web_page()
