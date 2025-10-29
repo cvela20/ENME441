cc843cbb37a5da68d44b12039bfdbe135b7facce
cvela20 <velacameron20@gmail.com>
2025-10-29 01:22:42 -0400

    Add files via upload

--- Lab8.py
+++ Lab8.py
@@ -83,7 +83,7 @@ def serve_web_page():
         conn.sendall(web_page())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 8080))
+s.bind(('', 80))
 s.listen(1)
 
 serve_web_page()
